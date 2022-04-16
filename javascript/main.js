800139f2b736cea390c3a5e2376b9f3e2d4e5e07
MateoGame26YT <mateogame26yt.oficial@gmail.com>
2022-04-16 11:21:44 -0300

    Initial commit

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,4 @@
+
 // Switch
 const btnSwitch = document.querySelector('#switch');
 
@@ -5,3 +6,29 @@ btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
 });
+
+// // Google
+
+// // Close de Window
+// const login = document.querySelector('#login');
+// const googleLoginCloseWindow = document.querySelector('#close-window');
+
+// googleLoginCloseWindow.addEventListener('click', () => {
+//     login.classList.remove('active');
+// });
+
+// // Login with Google
+// const btnGoogleLogin = document.querySelector('#google-register');
+
+// googleButton.addEventListener("click", (e) => {
+//     e.preventDefault()
+  
+//     const provider = new firebase.auth.GoogleAuthProvider();
+//     auth.signInWithPopup(provider).then((result) => {
+//         console.log(result);
+//         console.log("google sign in");
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+// });
