accf38d8133f07409ea9fd89743004547f8a7ba8
mateogasme <mateogame26yt.oficial@gmail.com>
2023-06-25 20:10:32 -0300

    Primer Commit

---
+++ javascript/main.js
@@ -0,0 +1,48 @@
+// FUNCIONALIDAD DE LA BARRA DE NAVEGACIÓN
+const btnToggle = document.querySelector('.toggle-btn');
+const blackScreen = document.querySelector('.black-screen');
+const btnCloseSidebar = document.querySelector('.btn-close-i');
+const liSidebar = document.querySelectorAll('#liSidebar');
+
+btnToggle.addEventListener('click', function () {
+    console.log('click');
+    document.getElementById('sidebar').classList.toggle('active');
+    console.log(document.getElementById('sidebar'));
+    blackScreen.classList.add('active');
+});
+
+blackScreen.addEventListener('click', function () {
+    document.getElementById('sidebar').classList.remove('active');
+    blackScreen.classList.remove('active');
+});
+
+btnCloseSidebar.addEventListener('click', function () {
+    document.getElementById('sidebar').classList.remove('active');
+    blackScreen.classList.remove('active');
+    console.log('click');
+});
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+        document.getElementById('sidebar').classList.remove('active');
+        blackScreen.classList.remove('active');
+    }
+});
+
+liSidebar.forEach(function (li) {
+    li.addEventListener('click', function () {
+        document.getElementById('sidebar').classList.remove('active');
+        blackScreen.classList.remove('active');
+    });
+});
+
+// COPIAR AL PORTAPAPELES EL E-MAIL
+const btnCopy = document.querySelector('#copy');
+
+copy.onclick = async () => {
+    try {
+        await navigator.clipboard.writeText('mateogasme@gmail.com');
+    } catch (error) {
+        console.error(error);
+    }
+}
